ba5c46fd5ab8868ed5def0961976d0d53b1fde96
luis leon <leonluis380@yahoo.com>
2021-05-31 17:36:35 -0400

    commit tree 4875ffe6f5e33a09455d4dc66db549253fba57df

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+   array.map.with_index(1) do |name, index|
+    puts "#{index} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(ary)
+  ary.map do |name|
+    name.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(ary)
+    ary.any? do |call|
+      call.size > 4 
+    
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+def find_the_cheese(ary)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+   ary.find do |cheese|
+    cheese_types.include?(cheese)
+
+   end
+  end
